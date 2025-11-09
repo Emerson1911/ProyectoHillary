445fcce9c14631e090d17138dfb6a985fae387c7
Emerson719 <1284662@clases.edu.sv>
2025-11-09 16:33:47 -0600

    Limpiar repositorio y aplicar .gitignore correctamente

--- Hillary.DTOs/TareaDTOS/CreateTareaDTO.cs
+++ Hillary.DTOs/TareaDTOS/CreateTareaDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Hillary.DTOs.TareaDTOS
+﻿namespace Hillary.DTOs.TareaDTOS
 {
     public class CreateTareaDTO
     {
--- Hillary.DTOs/UsuarioDTO/EditUsuarioDTO.cs
+++ Hillary.DTOs/UsuarioDTO/EditUsuarioDTO.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Hillary.DTOs.UsuarioDTO
+﻿namespace Hillary.DTOs.UsuarioDTO
 {
     public class EditUsuarioDTO
     {
         public string? Nombre { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
-       // public bool? Activo { get; set; }
+        // public bool? Activo { get; set; }
     }
 }
